95c71517f66a852fda31166fbe6d50e79197b934
47452636 <p.kools@student.ru.nl>
2024-03-17 14:43:31 +0100

    adjust plots

--- model_visualisation/LDM_visualisation.py
+++ model_visualisation/LDM_visualisation.py
@@ -152,12 +152,12 @@ def forecast_demo(
 
             fig = plt.figure(dpi=150)
 
-            ax1 = fig.add_subplot()
+            ax1 = fig.add_subplot(1, 2, 1)
             ax1.imshow(v_mean[0, 0, ...], cmap='gray')
             ax1.set_title(f'Mean of embedding after {k} block')
             ax1.axis('off')
 
-            ax2 = fig.add_subplot()
+            ax2 = fig.add_subplot(1, 2, 2)
             ax2.imshow(v_max[0, 0, ...], cmap='gray')
             ax2.set_title(f'Max of embedding after {k} block')
             ax2.axis('off')
