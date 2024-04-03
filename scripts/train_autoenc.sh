26bd0376429007e38c4d0784a93a5d1569864ac2
Pelle Kools <pkools@cn84.science.ru.nl>
2024-04-03 17:00:43 +0200

    set up script for training autoencoder on cluster

---
+++ scripts/train_autoenc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=cseduproject
+#SBATCH --partition=csedu
+#SBATCH --qos=csedu-large
+#SBATCH --mem=10G
+#SBATCH --gres=gpu:1
+#SBATCH --time=01:00:00
+#SBATCH --output=/vol/knmimo-nobackup/users/pkools/thesis-forecasting/ldcast/scripts/logs/train_autoenc-%j.out
+#SBATCH --error=/vol/knmimo-nobackup/users/pkools/thesis-forecasting/ldcast/scripts/logs/train_autoenc-%j.err
+
+source ../.ldcast_venv/bin/activate
+python train_autoenc.py
