512c03fedd53d69f021f0dbde87c5ee45237ebc8
Pelle Kools <pkools@cn84.science.ru.nl>
2024-04-03 14:23:46 +0200

    run demo with knmi data

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=cseduproject
 #SBATCH --partition=csedu
 #SBATCH --qos=csedu-large
-#SBATCH --mem=16G
+#SBATCH --mem=10G
 #SBATCH --gres=gpu:1
 #SBATCH --time=01:00:00
 #SBATCH --output=/vol/knmimo-nobackup/users/pkools/thesis-forecasting/ldcast/scripts/logs/myjob-%j.out
 #SBATCH --error=/vol/knmimo-nobackup/users/pkools/thesis-forecasting/ldcast/scripts/logs/myjob-%j.err
 
 source ../.ldcast_venv/bin/activate
-python forecast_demo.py --num_diffusion_iters=10
+python forecast_demo.py --t0="2019/10/28 11:25" --draw_border=False, --data_dir="../data/demo/knmi/" --num_diffusion_iters=10 --dataset_type="knmi"
